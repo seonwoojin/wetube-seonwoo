cf2eda910b1aee1a3ac07a419efc07171bab49e5
seonwoojin <wlstjsdn12@gmail.com>
2022-06-08 17:05:54 +0900

    2022-06-08

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("Hi");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleListening = () =>
+  console.log(`Server listenting on port htpp://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
