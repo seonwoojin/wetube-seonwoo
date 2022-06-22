f47cb04ae32eb853c33f6d5ad621c0c78fdfb948
Seonwoo <wlstjsdn12@gmail.com>
2022-06-22 23:19:27 +0900

    2022-06-22

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,30 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import {
+  edit,
+  remove,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+  getEdit,
+  postEdit,
+  getChangePassword,
+  postChangePassword,
+} from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter
+  .route("/change-password")
+  .all(protectorMiddleware)
+  .get(getChangePassword)
+  .post(postChangePassword);
 userRouter.get("/delete", remove);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
