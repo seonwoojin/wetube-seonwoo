d8525b4ba832577ee352cbacbe0dd2dcb966ea2f
Seonwoo <wlstjsdn12@gmail.com>
2022-08-31 13:44:46 +0900

    Update init.js

--- src/init.js
+++ src/init.js
@@ -8,6 +8,6 @@ import app from "./server";
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+  console.log(`✅ ServeR listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
